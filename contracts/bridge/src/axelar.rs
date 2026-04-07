574806ef5892cbe64bf972863b1e6c1be93fe8b8
Timi16 <timilehinolowu46@gmail.com>
2026-04-07 10:59:37 -0700

    Fix bridge payload string formatting

--- contracts/bridge/src/axelar.rs
+++ contracts/bridge/src/axelar.rs
@@ -1,12 +1,6 @@
 pub fn build_gmp_message(name: &str, destination_chain: &str, resolver: &str) -> String {
     format!(
-        concat!(
-            "{{",
-            "\"type\":\"xlm-ns-resolution\",",
-            "\"name\":\"{name}\",",
-            "\"destination_chain\":\"{destination_chain}\",",
-            "\"resolver\":\"{resolver}\"",
-            "}}"
-        )
+        "{{\"type\":\"xlm-ns-resolution\",\"name\":\"{}\",\"destination_chain\":\"{}\",\"resolver\":\"{}\"}}",
+        name, destination_chain, resolver
     )
 }
